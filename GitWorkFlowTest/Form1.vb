c9b39fc24379bcedd35c01bbbf0c18cb5f58e033
ล้ง Tungฯ <lonk.tung@hotmail.com>
2025-09-12 15:17:07 +0700

    REPI1

--- GitWorkFlowTest/Form1.vb
+++ GitWorkFlowTest/Form1.vb
@@ -7,18 +7,6 @@
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        'develop
-        'develop_b
-        'develop_a
-        'b 1
-        'b2
-
-        'A1
-        'D1
-
-        'b3
-        'b4
-        'RIST1
-
+        'REPI1
     End Sub
 End Class
